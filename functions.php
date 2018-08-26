f6d8b9c3383f36236dcc8fb14a89d47fd2906582
wimonsiri <wpakasame@gmail.com>
2018-08-26 22:29:01 +0700

    Enqueue bootstrap

--- functions.php
+++ functions.php
@@ -2,22 +2,22 @@
 
 add_action( 'after_setup_theme', 'wpse_theme_setup' );
 function wpse_theme_setup() {
-    /*
-     * Let WordPress manage the document title.
-     * By adding theme support, we declare that this theme does not use a
-     * hard-coded <title> tag in the document head, and expect WordPress to
-     * provide it for us.
-     */
-    add_theme_support( 'title-tag' );
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
 
-    /*
+	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
 
-    // Set the default content width.
+	// Set the default content width.
 	$GLOBALS['content_width'] = 525;
 
 	// Add default posts and comments RSS feed links to head.
@@ -29,3 +29,23 @@ function wpse_theme_setup() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
+
+function theme_styles() {
+
+	wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_styles');
+
+function theme_js() {
+
+	global $wp_scripts;
+
+	wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
+	wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '/js/scripts.js');
+
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_js');
